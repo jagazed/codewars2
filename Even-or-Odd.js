a614b25ab5b93009f48333665423be57505ab7c8
zelyankovas <vas.zelenko@gmail.com>
2024-03-26 19:37:36 +0300

    Even-or-Odd.js practice v2

--- Even-or-Odd.js
+++ Even-or-Odd.js
@@ -1,7 +1,7 @@
-function fits(x, y){
-    if (Number.isInteger(y / x)) {
-        return 'Fits';
+function evenOrOdd(number){
+    if (number % 2 === 0){
+        return "Even";
+    } else {
+        return "Odd";
     }
-    return 'Does not';
 }
-console.log(fits(5, 10));
