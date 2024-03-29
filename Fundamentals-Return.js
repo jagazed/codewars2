594b6004762408ff25c018ddd54fb0a43eee88f3
zelyankovas <vas.zelenko@gmail.com>
2024-03-29 16:14:13 +0300

    Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.

---
+++ Fundamentals-Return.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Make multiple functions that will return the sum, difference, modulus, product, quotient,
+// and the exponent respectively.
+//
+// MY SOLUTION
+function add(a,b){
+    return a + b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function mod(a,b){
+    return a % b;
+}
+
+function exponent(a,b){
+    return a ** b;
+}
+
+function subt(a,b){
+    return a - b;
+}
